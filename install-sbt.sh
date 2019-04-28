5ec809c1165c5c29684a8ed2dceaea9903ec6c6b
Vladyslav Pekker <agilesteel@gmail.com>
2019-04-28 12:23:20 +0300

    Update install-sbt.sh

--- install-sbt.sh
+++ install-sbt.sh
@@ -5,7 +5,7 @@ wget -q https://raw.githubusercontent.com/DevInsideYou/install-java/master/insta
 sudo chmod +x install-java.sh && ./install-java.sh
 
 # install sbt
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 sudo apt update
 sudo apt install -y sbt
