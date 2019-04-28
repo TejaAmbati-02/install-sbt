eb2d3cbf0af2981d127745e998615e40cc05b03e
Vladyslav Pekker <agilesteel@gmail.com>
2019-04-28 12:05:12 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Installs SBT and default-jre with default-jdk on Ubuntu
+# Installs [JRE](https://openjdk.java.net/), [JDK](https://openjdk.java.net/) & [SBT](https://www.scala-sbt.org/) on [Ubuntu](https://www.ubuntu.com/)
 
 ```bash
 wget -q https://raw.githubusercontent.com/DevInsideYou/install-sbt/master/install-sbt.sh
-sudo chmod +x install-sbt.sh
-./install-sbt.sh
+sudo chmod +x install-sbt.sh && ./install-sbt.sh
 ```
--- install-sbt.sh
+++ install-sbt.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
+# install jre & jdk
+wget -q https://raw.githubusercontent.com/DevInsideYou/install-java/master/install-java.sh
+sudo chmod +x install-java.sh && ./install-java.sh
+
 # install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install -y sbt
-
-# install java
-sudo apt-get install -y default-jre
-
-# install jdk
-sudo apt-get install -y default-jdk
-
-# show versions
-java -version
-javac -version
+sudo apt update
+sudo apt install -y sbt
 
 # remove yourself
 rm $0
