985bd6f5b9c5422bb4b8c4ad3ff7c97b36415bb4
Vladyslav Pekker <agilesteel@gmail.com>
2019-06-07 14:52:17 +0300

    Does not install dependencies (Java) any longer.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Installs [JRE](https://openjdk.java.net/), [JDK](https://openjdk.java.net/) & [SBT](https://www.scala-sbt.org/) on [Ubuntu](https://www.ubuntu.com/)
+# Installs [SBT](https://www.scala-sbt.org/) on [Ubuntu](https://www.ubuntu.com/)
 
 ```bash
 wget -q https://raw.githubusercontent.com/DevInsideYou/install-sbt/master/install-sbt.sh
--- install-sbt.sh
+++ install-sbt.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# install jre & jdk
-wget -q https://raw.githubusercontent.com/DevInsideYou/install-java/master/install-java.sh
-sudo chmod +x install-java.sh && ./install-java.sh
-
 # install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
