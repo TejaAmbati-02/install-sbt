2a197494f7cd59d39df16bb310856b9d57a41885
Vladyslav Pekker <agilesteel@gmail.com>
2020-10-04 15:36:50 -0300

    housekeeping

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Installs latest version of [SBT](https://www.scala-sbt.org/) on [Ubuntu](https://www.ubuntu.com/)
+### Have these installed before you start:
+* [Coursier CLI](https://github.com/DevInsideYou/install-coursier-cli)
+
+# Installs [SBT](https://www.scala-sbt.org/) on [Ubuntu](https://www.ubuntu.com/)
 
 ```bash
 wget -q https://script.install.devinsideyou.com/sbt
-sudo chmod +x sbt && ./sbt
+chmod +x sbt && ./sbt
 ```
 
 ### Runtime Dependencies:
--- install-sbt.sh
+++ install-sbt.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# install sbt
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt update
-sudo apt install -y sbt
-
-echo
-
-echo '"sbt" is now on the path'
-
 # remove yourself
 rm $0
+
+# install sbt
+cs install sbt
+
+echo
+echo '"sbt" is now on the path (assuming coursier bin directory is also on the path)'
