3f489f9aa14c1843b9421e8b62564ec7ba9334dc
TejaAmbati <73156927+TejaAmbati-02@users.noreply.github.com>
2023-04-22 12:09:22 +0530

    Create old-install-sbt.sh

---
+++ old-install-sbt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+
+# install java
+sudo apt-get install -y default-jre
+
+# install jdk
+sudo apt-get install -y default-dk
+
+#Show versions
+javac -version
+java -version
+
+# remove yourself
+rm $0
